61d74b1a2ecbcb9f116c89bbe5faec88b99dcd73
Fernando D. Gómez <fgomezdev@gmail.com>
2024-02-14 15:53:28 -0300

    normalize searched text for catalogs

--- src/utils/helpers.py
+++ src/utils/helpers.py
@@ -18,3 +18,47 @@ def tr(message):
     """Get the translation for a string using Qt translation API."""
 
     return QCoreApplication.translate('@default', message)
+
+
+def normalize_text(text):
+    """Remove accents from text."""
+
+    accents_map = {
+        'á': 'a',
+        'é': 'e',
+        'í': 'i',
+        'ó': 'o',
+        'ú': 'u',
+        'à': 'a',
+        'è': 'e',
+        'ì': 'i',
+        'ò': 'o',
+        'ù': 'u',
+        'â': 'a',
+        'ê': 'e',
+        'î': 'i',
+        'ô': 'o',
+        'û': 'u',
+        'ä': 'a',
+        'ë': 'e',
+        'ï': 'i',
+        'ö': 'o',
+        'ü': 'u',
+        'ã': 'a',
+        'ñ': 'n',
+        'õ': 'o',
+        'ç': 'c',
+        'å': 'a',
+        'æ': 'ae',
+        'ø': 'o',
+        'ß': 'ss',
+    }
+
+    outputString = ''
+    for char in text:
+        if char in accents_map:
+            outputString += accents_map[char]
+        else:
+            outputString += char
+
+    return outputString
